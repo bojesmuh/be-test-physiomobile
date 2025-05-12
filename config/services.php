7a557987a002950f12b90af9bcf9f798d73d240b
Muhammad Azis S <muhammadaziss@Muhammads-MacBook-Pro.local>
2025-05-12 22:59:12 +0700

    add access key

--- config/services.php
+++ config/services.php
@@ -34,5 +34,6 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'api_key' => env('API_KEY'),
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
+use App\Http\Middleware\CheckApiKey;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('/patiens', PatientController::class);
+Route::apiResource('/patiens', PatientController::class)->middleware(CheckApiKey::class);
